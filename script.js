b77af5cdaf29e045a318d5a168fc85bac0e3e0d9
amireous <82820601+amireous@users.noreply.github.com>
2021-07-22 18:01:04 +0430

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,9 @@ const signForm = document.querySelector(".signup-form");
 const loginForm = document.querySelector(".login-form");
 
 const signReminder = document.querySelector(".sign-reminder");
-const Loginbtn = document.querySelector(".btn");
-const userMail = document.getElementById("email-login");
-const userPass = document.getElementById("pass-login");
-const closeModal = document.querySelectorAll(".close-modal");
 
-const image = document.querySelector("img");
+
+
 
 signupSection.addEventListener("click", function () {
   loginSection.classList.add("reset-colors");
@@ -39,27 +36,4 @@ loginSection.addEventListener("click", function () {
   signReminder.classList.remove("hidden");
 });
 
-const userInformaton = [];
 
-Loginbtn.addEventListener("click", function (event) {
-  event.preventDefault(); // to stop the form submittig
-
-  let userInfo = {
-    email: userMail.value,
-    password: userPass.value,
-  };
-  userInformaton.push(userInfo);
-  document.querySelector("form").reset();
-  console.log(userInformaton);
-
-  if (userInfo.email == "shahinkoonde@hasti.com" && userInfo.password == 8585) {
-    container.classList.add("blur");
-    image.classList.remove("hidden");
-    closeModal.classList.remove("hidden");
-  }
-});
-
-closeModal.addEventListener("click", function () {
-  image.classList.add("hidden");
-  container.classList.remove("hidden");
-});
